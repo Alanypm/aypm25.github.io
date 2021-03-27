3833c581d84375552c35f27dd92b3ac4ff40d320
Alanypm <81442841+Alanypm@users.noreply.github.com>
2021-03-26 21:35:45 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,9 @@
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyAacJ9EXEi0vA7JiegdITxCV24TTNjHcuA",
+  authDomain: "iotaypm.firebaseapp.com",
+  projectId: "iotaypm",
+  storageBucket: "iotaypm.appspot.com",
+  messagingSenderId: "1035622274067",
+  appId: "1:1035622274067:web:a214075e5f991b2c8d1be6"
+});
